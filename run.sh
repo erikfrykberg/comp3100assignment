5e394e907402c59d5806c4c1f0ba507998a80e70
Erik <erik.frykberg@gmail.com>
2022-04-04 11:57:08 +1000

    changed message

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "This script is about to run another script."
+echo "RUNNING ERIK FRYKBERG'S DSClient.class!"
 sh ./demoS1.sh -n DSClient.class
-echo "This script has just run another script."
+echo "Successful! Well done."
