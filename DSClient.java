4da329f9a2151af38ae3a70e1d67fb831e5fc649
Erik <erik.frykberg@gmail.com>
2022-04-04 10:41:10 +1000

    comments + improved readability

--- DSClient.java
+++ DSClient.java
@@ -51,13 +51,13 @@ public class DSClient {
         */
 
         //JOB VARIABLES
-        String jobId; 
-        String jobCommand;
+        String jobId; //we need to save the id of each job.
+        String jobCommand; //we need to observe the command recieved with each job.
 
         // SERVER VARIABLES
         String largestType = ""; //store the largest type.
-        int numberOfServers = 0;
-        int index = 0;
+        int numberOfServers = 0; //store number of largest type.
+        int index = 0; //track which was the last server we scheduled too.
 
         System.out.println(" ---<>--- str equals: " + str + " ---<>--- \n");
         //WHILE THERE ARE JOBS TO SCHEDULE.
@@ -69,36 +69,42 @@ public class DSClient {
             //EXPECT JOBN or NONE RESPONSE
             recieve();
 
+            //if there are no more jobs, we must exit.
             if(str.equals("NONE")) {
                 break;
             }
 
             //the following is the JOB breakdown.
             String[] jobStrings = str.split(" "); 
-            jobCommand = jobStrings[0];
-            jobId = jobStrings[2];
+            jobCommand = jobStrings[0]; //set job command.
+            jobId = jobStrings[2]; //set job id.
+
+            /**
+             * 
+             *  SAVE THE LARGEST SERVER TYPE AND NUMBER
+             * 
+            */
 
             //if the largest server type has not been set, then:
             System.out.println(" ---<>--- largest type equals: " + largestType + " ---<>---\n");
             if(largestType.equals("")) {
                 
-                String coresRequired = jobStrings[jobStrings.length - 3];
-                String memoryRequired = jobStrings[jobStrings.length - 2];
-                String disksRequired = jobStrings[jobStrings.length - 1];
+                String coresRequired = jobStrings[jobStrings.length - 3]; //save cores required.
+                String memoryRequired = jobStrings[jobStrings.length - 2]; //save memory required.
+                String disksRequired = jobStrings[jobStrings.length - 1]; //save disks required.
 
                 //REQUEST SERVERS
                 push("GETS Capable " + coresRequired + " " + memoryRequired + " " + disksRequired);
     
                 //RECIEVE THE DATA [number] [length of characters].
                 recieve();
-                Integer totalServers = Integer.parseInt(str.split(" ")[1]);
+                Integer totalServers = Integer.parseInt(str.split(" ")[1]); //save total servers from response.
                 System.out.println(" ---<>--- There should be: " + str.split(" ")[1] + " number of servers ---<>--- \n");
     
                 //SEND 'OK'
                 push("OK");
     
-                int mostCores = 0;
-                String currentType = "";
+                int mostCores = 0; //local variable for current most cores.
                 //RECIEVE THE SERVERS - MUST BE AT LEAST 1!
                 for(int i = 0; i < totalServers; i++){
                     recieve();
@@ -110,14 +116,14 @@ public class DSClient {
                         largestType = serverInformation[0]; //set to the biggest type
                         numberOfServers = 0; //reset the number of servers.
                         mostCores = cores; //set the new mostCores.
-                        currentType = serverInformation[0]; //set the current type we're looking for.
                     }
                     //increase number of servers if of the same type.
-                    if(serverInformation[0].equals(currentType)){
+                    if(serverInformation[0].equals(largestType)){
                         numberOfServers++;
                     }
                 }
 
+                //push OK after recieving all of the servers.
                 push("OK");
 
                 //RECIEVE '.'
@@ -133,6 +139,7 @@ public class DSClient {
             //check to make sure that it is a normal JOB.
             if(jobCommand.equals("JOBN")){
                 if(index == numberOfServers){
+                    //schedule from 0 again if we have reached the end of the servers and there are more jobs.
                     index = 0;
                 }
                 System.out.println("---<>--- number of servers: " + numberOfServers + "\n");
