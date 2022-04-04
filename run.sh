181a301dac0487b81d2a374fefc372862c5711ab
Erik <erik.frykberg@gmail.com>
2022-04-04 11:48:41 +1000

    changed the script call

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "This script is about to run another script."
-sh ./demoS1.sh -n ./DSClient.class
+sh ./demoS1.sh -n DSClient.class
 echo "This script has just run another script."
